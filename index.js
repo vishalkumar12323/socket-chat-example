a51c30baf69ff6a9fd70ecfcbbcf4a66ef25c4db
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-05-28 22:18:28 +0530

    pass html through index.html

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import { createServer } from "node:http";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url"
 
 
 const port = Number(process.env.PORT) || 3001;
 const app = express();
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const server = createServer(app);
 
 app.get("/", (req, res) => {
-    res.send(`<h1> Hello World! </h1>`);
+    res.sendFile(join(__dirname, "index.html"));
 });
 
 server.listen(port, () => console.log(`Express + Socket.io server running on port: ${port}`));
