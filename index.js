49f78ebd318e483888decc2577aff5a2b8d5e2e6
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-05-28 22:53:02 +0530

    create message event and test

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { createServer } from "node:http";
 import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url"
+import { fileURLToPath } from "node:url";
+import { Server } from "socket.io";
 
 
 const port = Number(process.env.PORT) || 3001;
@@ -11,8 +12,24 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const server = createServer(app);
 
+const io = new Server(server)
+
 app.get("/", (req, res) => {
     res.sendFile(join(__dirname, "index.html"));
+});
+
+
+io.on('connection', (socket) => {
+    console.log(`A new user connected:: ${socket.id}`);
+
+    socket.on('message', (data) => {
+        console.log(`message received:: ${data}`);
+    });
+
+
+    socket.on('disconnect', () => {
+        console.log(`User disconnected:: ${socket.id}`);
+    });
 });
 
 server.listen(port, () => console.log(`Express + Socket.io server running on port: ${port}`));
