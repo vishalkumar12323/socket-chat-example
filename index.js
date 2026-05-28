684a4345e482e4b32ad6eb47947eb95c11c010b9
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-05-28 22:07:17 +0530

    setup express and http server

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { createServer } from "node:http";
+
+
+const port = Number(process.env.PORT) || 30001;
+const app = express();
+
+const server = createServer(app);
+
+app.get("/", (req, res) => {
+    res.send(`<h1> Hello World! </h1>`);
+});
+
+server.listen(port, () => console.log(`Express + Socket.io server running on port: ${port}`));
