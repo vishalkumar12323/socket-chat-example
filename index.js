405433f1bd88a2b7abeeb1613d9a0f81a5289542
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-06-01 16:34:06 +0530

    intgrate data persist with sqlite

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ import { createServer } from "node:http";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 import { Server } from "socket.io";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
+
+const db = await open({
+    filename: 'chat.db',
+    dirname: sqlite3.Database
+});
+
+
+await db.exec(`
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            client_offset TEXT UNIQUE,
+            content TEXT
+        );
+    `)
 
 const port = Number(process.env.PORT) || 3001;
 const app = express();
@@ -24,9 +40,15 @@ app.get("/", (req, res) => {
 io.on('connection', (socket) => {
     console.log(`A new user connected:: ${socket.id}`);
 
-    socket.on('message', (data) => {
-        // console.log(`message received:: ${data}`);
-        io.emit("chat_msg", data);
+    socket.on('message', async (msg) => {
+        let result;
+        try {
+            result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+        } catch (err) {
+            console.log("MSG INSERT ERR:: ", err);
+            return;
+        }
+        io.emit("chat_msg", msg, result.lastID);
 
         // socket.broadcast.emit("chat_msg", data);
     });
